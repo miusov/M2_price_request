3dc63875eba1059e7fa732c32a8600ce78c9b3a2
artem_miusov <miusov86@gmail.com>
2018-11-15 21:45:08 +0200

    create module sceleton

---
+++ app/code/AM/PriceRequest/registration.php
@@ -0,0 +1,27 @@
+<?php
+/**
+​ * ​ ​ Webinse
+​ *
+​ * ​ ​ PHP​ ​ Version​ ​ 7.1.24
+​ *
+​ * ​ ​ @category     Extensions
+​ * ​ ​ @package    ​ ​ AM_PriceRequest
+​ * ​ ​ @author       Artem Miusov <miusov86@gmail.com>
+​ * ​ ​ @copyright  ​ ​ ${YEAR} ​ Artem Miusov
+​ * ​ ​ @license    ​ ​ http://opensource.org/licenses/OSL-3.0​ ​ The​ ​ Open​ ​ Software​ ​ License​ ​ 3.0
+​ */
+/**
+​ * ​ ​ Comment​ ​ for​ ​ file
+​ *
+​ * ​ ​ @category     Extensions
+​ * ​ ​ @package    ​ ​ AM_PriceRequest
+​ * ​ ​ @author       Artem Miusov <miusov86@gmail.com>
+​ * ​ ​ @copyright  ​ ​ ${YEAR} ​ Artem Miusov
+​ * ​ ​ @license    ​ ​ http://opensource.org/licenses/OSL-3.0​ ​ The​ ​ Open​ ​ Software​ ​ License​ ​ 3.0
+​ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'AM_PriceRequest',
+    __DIR__
+);
